83f3b8b43dc8fc52de5e9b83796891696ce8769b
MaxChevalier <maxime.chevalier@ynov.com>
2025-06-15 12:42:47 +0200

    add class model

---
+++ src/shared/model/class.model.ts
@@ -0,0 +1,18 @@
+import { Ability } from "./ability.model";
+
+export interface Class {
+    id: bigint;
+    name: string;
+    casterType: string;
+    hitDie: number;
+    savingThrows: Ability[];
+    subClass?: bigint;
+}
+
+export interface ClassCandidate {
+    name: string;
+    casterType: string;
+    hitDie: number;
+    savingThrows: bigint[];
+    subClass?: bigint;
+}
--- src/shared/model/models.ts
+++ src/shared/model/models.ts
@@ -5,3 +5,4 @@ export { Skill, SkillCandidate } from './skill.model';
 export { Background, BackgroundCandidate, BackgroundAbility, BackgroundAbilityCandidate, BackgroundSkill, BackgroundSkillCandidate, BackgroundLanguage } from './background.model';
 export { Traits, TraitsCandidate } from './traits.model';
 export { Race, RaceCandidate } from './race.model';
+export { Class, ClassCandidate } from './class.model';
